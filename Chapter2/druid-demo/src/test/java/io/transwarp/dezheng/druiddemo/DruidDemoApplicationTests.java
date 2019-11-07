5b21826fa671352de23b3c52412d20f4ca4345d7
Tom Wang <wang793013053@163.com>
2019-11-08 02:29:53 +0800

    Chapter2-demo08: add druid-demo

---
+++ Chapter2/druid-demo/src/test/java/io/transwarp/dezheng/druiddemo/DruidDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.transwarp.dezheng.druiddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DruidDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
